fb47b1228e2a44c657a641ea400e1e70b9276177
AndrySten <stenyakin17@gmail.com>
2020-06-09 14:56:07 +0300

    Laboratory 10

---
+++ Stenyakin/laboratory10/src/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './MyInfo.css'
+function MyInfo() {
+    return (
+<div>
+<h1>Андрей Стенякин</h1>
+<p>Меня зовут Андрей Стенякин.</p>
+      <p>Живу в городе Харькове. Люблю заниматься спортом</p>
+<p>Страны в которых я хочу побывать:</p>
+    <ul>
+      <li>Англия</li>
+      <li>Германия</li>
+      <li>Италия</li>
+</ul>
+</div>
+        )
+}
+
+export default MyInfo
